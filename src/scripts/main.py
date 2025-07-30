0cafe7b168426df5bcb6e0cbddbe7dfca507dd82
mengsig <marcus.engsig@tii.ae>
2025-07-30 12:48:50 +0400

    ruff linting + added main script

---
+++ src/scripts/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import sys
+import os
+
+# Append the utils directory to the system path for importing PlotDefaults.
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+from src.utils.plot_defaults import plot_default
